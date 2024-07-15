b80848fa17d9b6e156c6d22b9061cfbd30a8307e
GokulPrasathR <gokulprasathr.22bir@kongu.edu>
2024-07-15 15:05:22 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by&nbsp;&nbsp;
+      <a href="https://github.com/GokulPrasathR"></a>
+        <a>Gokul Prasath R</a>&nbsp;&nbsp;
+        To View an Website&nbsp;&nbsp;
+       <a href="https://github.com/GokulPrasathR">Open sourced on Github.&nbsp;</a> Hosted on Git
+    </p>
+  </div>
+  );
+}
